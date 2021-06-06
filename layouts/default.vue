8c86ed78713c508ac6d13f80caf14c963e40ef0d
Josep V. Monjo <jvmonjo@gmail.com>
2021-06-06 14:30:05 +0200

    ADDED: sections and content

--- layouts/default.vue
+++ layouts/default.vue
@@ -48,6 +48,17 @@
                 <p class="subtitle text-white">
                   Una app de codi obert a l'abast de tothom
                 </p>
+                <p class="text-white">
+                  Si a la vostra AMPA no teniu ningú capacitat per implementar aquesta app, pode fer-vos un pressupost per implementar-la nosaltres
+                </p>
+                <br>
+                <a class="button is-white is-outlined" href="mailto:jvmonjo@gmail.com" target="_blank">
+                  <b-icon
+                    icon="email-outline"
+                    size="is-small"
+                  />
+                  <strong>Contacta'ns</strong>
+                </a>
               </div>
               <div class="column is-bottom">
                 <img src="@/assets/mum.png" alt="">
@@ -61,7 +72,35 @@
       <Nuxt />
     </main>
     <footer>
-      footer
+      <div class="container">
+        <div class="content">
+          <div class="columns">
+            <div class="column">
+              <h3 class="text-white">
+                AMPA PWA
+              </h3><div class="buttons">
+                <a class="button is-primary" style="color:white; background:#0f3946;" href="https://github.com/fampa/ampa-pwa" target="_blank">
+                  <b-icon
+                    icon="github"
+                    size="is-small"
+                  />
+                  <strong>Github</strong>
+                </a>
+                <a class="button is-white is-outlined" href="https://fampa-pwa.web.app" target="_blank">
+                  <b-icon
+                    icon="cellphone-text"
+                    size="is-small"
+                  />
+                  <strong>App d'exemple</strong>
+                </a>
+              </div>
+            </div>
+          </div>
+          <p class="text-white">
+            Projecte de Final del <strong>Màster de Desenvolupament d'Aplicacions i Llocs Web</strong> de la UOC realitzat per <strong>Josep Vicent Monjo i Agut</strong>.
+          </p>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
@@ -89,15 +128,24 @@ export default {
 </script>
 
 <style>
-.text-white{
+footer {
+  font-size: 0.8rem;
+}
+html {
+  background-color: "#1a667e";
+}
+@media screen and (max-width: 1023px) {
+  .navbar-menu {
+    background: linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.3));
+  }
+}
+.text-white, .text-white strong{
   color: white !important;
 }
+
 nav.navbar.is-transparent {
     background: transparent;
     color: white;
-    position: fixed;
-    top: 0;
-    left: 0;
     width: 100%;
 }
 
@@ -105,6 +153,8 @@ nav.navbar.is-transparent a{
     color: white;
 }
 .heroAlt {
+  margin-top: -3.3rem;
+  padding-top: 1rem;
   background: linear-gradient(rgba(26, 102, 126, 1), rgba(26, 102, 126, 1),rgba(26, 102, 126, 0.9), rgba(26, 102, 126, 0.6));
 }
 a.navbar-burger.burger {
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,105 @@
 <template>
-  <section>
-    <div class="container">
-      <div class="content">
-        <div class="columns">
-          <div class="column">
-            <h2>Codi obert</h2>
-            <p>AMPA PWA és una aplicació de codi obert</p>
-          </div>
-          <div class="column">
-            2
-          </div>
-          <div class="column">
-            3
+  <div class="bg-white">
+    <section>
+      <div class="container">
+        <div class="content">
+          <div class="columns">
+            <div class="column">
+              <h2>Codi obert</h2>
+              <p>AMPA PWA és una aplicació de codi obert. Modifica-la com vulguis</p>
+              <div class="text-center">
+                <img src="@/assets/opensource.png" alt="">
+              </div>
+            </div>
+            <div class="column">
+              <h2>Aplicació Web Progressiva (PWA)</h2>
+              <p>Es tracta d'una aplicació web que s'adapta a qualsevol mida de pantalla i dispositiu i permet instal·lar-se i rebre notificacions</p>
+              <div class="text-center">
+                <img src="@/assets/pwa.png" alt="">
+              </div>
+            </div>
+            <div class="column">
+              <h2>Gratis!</h2>
+              <p>La majoria d'AMPA no requereixen molt recursos per funcionar, i normalment serà suficient usant serveis gratuïts al núvol, però també podeu autoallotjar-la</p>
+              <div class="text-center">
+                <img src="@/assets/payment.png" alt="">
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+    <section class="bg-grey">
+      <div class="container">
+        <div class="content">
+          <div class="columns">
+            <div class="column">
+              <h2>Menú d'administrador</h2>
+              <img class="capture is-center" src="@/assets/menu-normal.png" alt="">
+              <img class="capture is-center" src="@/assets/menu-admin.png" alt="">
+            </div>
+            <div class="column is-center">
+              <p class="featured">
+                Segons els permisos de l'usuari aquest tindrà accés a unes funcions o altres
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="content">
+          <div class="columns">
+            <div class="column is-center">
+              <p class="featured">
+                Gestiona altes i baixes, enviament de missatges per correu electrònic i notificacions push
+              </p>
+            </div>
+            <div class="column">
+              <h2>Gestió de membres</h2>
+              <img class="capture is-center" src="@/assets/envia-missatge.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-grey">
+      <div class="container">
+        <div class="content">
+          <div class="columns">
+            <div class="column">
+              <h2>Gestió de continguts</h2>
+              <img class="capture is-center" src="@/assets/gestio-contingut.png" alt="">
+              <img class="capture is-center" src="@/assets/gestio-contingut-2.png" alt="">
+            </div>
+            <div class="column is-center">
+              <p class="featured">
+                Gestiona articles del blog, pàgines, serveis, etiquetes i tot de manera visual i multidioma
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="content">
+          <div class="columns">
+            <div class="column is-center">
+              <p class="featured">
+                Gestiona les domiciliacions bancàries, la signatura digital del document manament de domiciliacions així com el fitxer de remesa bancària
+              </p>
+            </div>
+            <div class="column">
+              <h2>Gestió de domiciliacions <br> <span class="dev">en desenvolupament</span> </h2>
+              <img class="capture is-center" src="@/assets/gestio-remesa.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -26,9 +109,36 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+.dev {
+  opacity: 0.8;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+}
+.bg-white {
+  background-color: white;
+}
+.bg-grey {
+  background-color: rgb(243, 243, 243);
+}
+.featured {
+  font-size: 1.8rem;
+}
+.text-right {
+  text-align: right;
+}
+.capture {
+  max-height: 400px;
+  max-width: 45%;
+}
 .has-bg-img {
   background: url('/assets/images/img.jpg')center center;
   background-size:cover;
+}
+.text-center {
+  text-align: center;
+}
+.container {
+  padding: 2rem;
 }
 </style>
